2af1cca96e17e3374e6890cb74f3a13582b2e43f
Damyres <damyresmaciell@gmail.com>
2024-03-15 21:13:06 -0300

    aula - entidade departamento

---
+++ src/application/Programa.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Departamento;
+import org.w3c.dom.ls.LSOutput;
+
+public class Programa {
+    public static void main(String[] args) {
+        Departamento obj = new Departamento(1, "Livros");
+        System.out.println(obj);
+
+    }
+
+}
